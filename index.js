9602f02f9abd79bc5c79c4e635519bfa5a629dd6
zeinab <swarsama@hotmail.co.uk>
2024-04-14 16:36:45 +0100

    fixing the post

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 import express from "express";
 import dotenv from "dotenv";
+import { Product } from "./models/product.js";
 
 dotenv.config();
 
@@ -14,10 +15,16 @@ app.get("/", (req, res) => {
   res.send("Hello, from API")
 });
 
-app.post("/api/products", (req, res) => {
-  console.log(req.body)
-  res.send(req.body)
-})
+app.post("/api/products", async (req, res) => {
+  // console.log(req.body)
+  // res.send(req.body)
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 //mongoDB connect then listen to app server at port
 mongoose.connect(process.env.MongoDB)
