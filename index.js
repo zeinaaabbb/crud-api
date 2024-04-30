6c24fd4ba7c579cc63663a927c355930b462d499
zeinab <swarsama@hotmail.co.uk>
2024-04-30 19:50:11 +0100

    Revert "updated gitignore"
    This reverts commit fda62044ac12f406787b14533b480cd5e7caf20d.

--- index.js
+++ index.js
@@ -6,18 +6,10 @@ dotenv.config();
 
 const app = express();
 
-//middleware
-app.use(express.json());
-
 
 app.get("/", (req, res) => {
   res.send("Hello, from API")
 });
-
-app.post("/api/products", (req, res) => {
-  console.log(req.body)
-  res.send(req.body)
-})
 
 //mongoDB connect then listen to app server at port
 mongoose.connect(process.env.MongoDB)
