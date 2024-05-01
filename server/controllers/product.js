6e154be8c47e632b99b2c103cb71cc8e51a04c93
zeinab <swarsama@hotmail.co.uk>
2024-05-01 22:37:06 +0100

    created router and controlled logic

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -11,6 +11,63 @@ const getProducts = async(req, res) => {
   }
 }
 
+const getProduct = async(re,res) => {
+  try {
+    const {id} = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const createProduct = async(req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const updateProduct = async(req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+
+    if (!product) {
+      return res.status(404).json({ message: "product does not exist!"})
+    }
+
+    const updatedProduct = await Product.findById(id);
+    res.status(200).json(updatedProduct);
+
+
+  } catch(error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const deleteProduct = async(req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product does not exist"})
+    }
+    res.status(200).json({ message: "Product has now been deleted"})
+
+  } catch (error) {
+    res.status(500).json({ message: error.message})
+  }
+}
+
+
 module.exports = {
-  getProducts
+  getProducts,
+  getProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct,
 }
--- server/routes/product.js
+++ server/routes/product.js
@@ -1,5 +1,12 @@
 import express from "express";
 const router = express.Router();
-const { getProducts } = require("../controllers/product.js")
+import { Product } from "../models/product.js";
+import { getProducts, getProduct, createProduct, updateProduct, deleteProduct } from "../controllers/product.js"
 
 router.get("/", getProducts);
+router.get("/:id", getProduct);
+router.post("/", createProduct);
+router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
+
+module.exports = router;
